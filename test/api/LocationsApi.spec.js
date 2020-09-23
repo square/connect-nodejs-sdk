08516728bdc855a23e5f2fc3334a7ea931959f04
Jessie De La Cruz Santos <jessiedlcs@squareup.com>
2020-09-23 11:35:02 -0700

    Fix Tests

--- test/api/LocationsApi.spec.js
+++ test/api/LocationsApi.spec.js
@@ -9,7 +9,7 @@ describe('LocationsApi', function () {
   beforeEach(function () {
     const defaultClient = SquareConnect.ApiClient.instance;
     const oauth2 = defaultClient.authentications['oauth2'];
-    oauth2.accessToken = accounts.sandbox.access_token;
+    oauth2.accessToken = accounts.production.access_token;
     this.api = new SquareConnect.LocationsApi();
   });
 
